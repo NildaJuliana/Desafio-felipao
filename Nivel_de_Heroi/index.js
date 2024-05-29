90bf955ce4798473b38d84c4c04111ba199ba525
NildaJuliana <nildajulianaor@gmail.com>
2024-05-29 20:05:46 -0300

    versao 1.0 do código

---
+++ Nivel_de_Heroi/index.js
@@ -0,0 +1,32 @@
+const readline = require('readline-sync');
+
+let nomeHeroi = readline.question("Herói, informe seu nome: ");
+console.log("Olá, meu nome é " + nomeHeroi);
+
+let XP = readline.question("Agora informe sua quantidade de experiência: ");
+XP = parseInt(XP)
+
+if (XP <= 1000) {
+    console.log("O Herói " + nomeHeroi + "está no nível Ferro!")
+}
+else if (XP >= 1001 && XP < 2001) {
+    console.log("O Herói" + nomeHeroi + "está no nível Bronze!")
+}
+else if (XP >= 2001 && XP < 5001) {
+    console.log("O Herói" + nomeHeroi + "está no nível prata!")
+}
+else if (XP >= 5001 && XP < 7001) {
+    console.log("O Herói" + nomeHeroi + "está no nível Ouro!")
+}
+else if (XP >= 7001 && XP < 8001) {
+    console.log("O Herói" + nomeHeroi + "está no nível Platina!")
+}
+else if (XP >= 8001 && XP < 9001) {
+    console.log("O Herói" + nomeHeroi + "está no nível Ascendente!")
+}
+else if (XP >= 9001 && XP < 10001) {
+    console.log("O Herói" + nomeHeroi + "está no nível Imortal!")
+}
+else if (XP >= 10001) {
+    console.log("O Herói" + nomeHeroi + "está no nível Radiante!")
+}
