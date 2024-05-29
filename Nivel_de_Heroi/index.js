993cc2a850b4966f63ef333098cb235bd202713f
NildaJuliana <nildajulianaor@gmail.com>
2024-05-29 20:26:17 -0300

    código final do desafio

--- Nivel_de_Heroi/index.js
+++ Nivel_de_Heroi/index.js
@@ -1,32 +1,32 @@
 const readline = require('readline-sync');
 
-let nomeHeroi = readline.question("Herói, informe seu nome: ");
-console.log("Olá, meu nome é " + nomeHeroi);
+let nomeHeroi = readline.question("Heroi, informe seu nome: ");
+console.log("Seja bem-vindo, " + nomeHeroi);
 
-let XP = readline.question("Agora informe sua quantidade de experiência: ");
+let XP = readline.question("Agora informe sua quantidade de experiencia: ");
 XP = parseInt(XP)
 
 if (XP <= 1000) {
-    console.log("O Herói " + nomeHeroi + "está no nível Ferro!")
+    console.log("O Herói " + nomeHeroi + " está no nível Ferro!")
 }
 else if (XP >= 1001 && XP < 2001) {
-    console.log("O Herói" + nomeHeroi + "está no nível Bronze!")
+    console.log("O Herói " + nomeHeroi + " está no nível Bronze!")
 }
 else if (XP >= 2001 && XP < 5001) {
-    console.log("O Herói" + nomeHeroi + "está no nível prata!")
+    console.log("O Herói " + nomeHeroi + " está no nível prata!")
 }
 else if (XP >= 5001 && XP < 7001) {
-    console.log("O Herói" + nomeHeroi + "está no nível Ouro!")
+    console.log("O Herói " + nomeHeroi + " está no nível Ouro!")
 }
 else if (XP >= 7001 && XP < 8001) {
-    console.log("O Herói" + nomeHeroi + "está no nível Platina!")
+    console.log("O Herói " + nomeHeroi + " está no nível Platina!")
 }
 else if (XP >= 8001 && XP < 9001) {
-    console.log("O Herói" + nomeHeroi + "está no nível Ascendente!")
+    console.log("O Herói " + nomeHeroi + " está no nível Ascendente!")
 }
 else if (XP >= 9001 && XP < 10001) {
-    console.log("O Herói" + nomeHeroi + "está no nível Imortal!")
+    console.log("O Herói " + nomeHeroi + " está no nível Imortal!")
 }
 else if (XP >= 10001) {
-    console.log("O Herói" + nomeHeroi + "está no nível Radiante!")
+    console.log("O Herói " + nomeHeroi + " está no nível Radiante!")
 }
